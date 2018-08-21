ac6dfadc7b9802061c75cb8b5bc8b7d445d5d97d
ankushbandil12 <42575929+ankushbandil12@users.noreply.github.com>
2018-08-21 18:14:37 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if($method == "POST") {
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+	
+	$text = $json->result->parameters->text;
+	
+	switch ($text) {
+		case 'hi':
+			$speech = "hey";
+			break;
+			
+		default:
+			$speech = "Default";
+			break;
+	}
+	
+	$response = new \stdClass();
+	$response->speech = "";
+	$response->displayText="";
+	$response->source = "webhook";
+	echo json_encode($response);
+	
+}
+else 
+{
+	echo "Method not allowed"
+}
+
+?>
