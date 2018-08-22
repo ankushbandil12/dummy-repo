a17cd4429256e9c7a5391406023de7ece32e4d86
ankushbandil12 <42575929+ankushbandil12@users.noreply.github.com>
2018-08-22 13:03:52 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "POST") {
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+	
+	$text = $json->result->parameters->text;
+	
+	switch ($text) {
+		case 'hi':
+			$speech = "hey";
+			break;
+			
+		default:
+			$speech = "Default";
+			break;
+	}
+	
+	$response = new \stdClass();
+	$response->fulfillmentText = "This response is coming from Heroku";
+	#$response->fulfillmentMessages=[""];
+	$response->source = "webhook";
+	#$response->payload = "";
+	#$response->outputContexts = [""];
+	#$response->followupEventInput = "";
+	echo json_encode($response);
+	
+}
+else 
+{
+	echo "Method not allowed";
+}
+?>
